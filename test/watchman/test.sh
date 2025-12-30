e685ce51f916d7f14468082831234c424402e988
Carlos Eduardo Monti <slaytanic@gmail.com>
2025-12-30 10:03:13 +0100

    test

---
+++ test/watchman/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This test file will be executed against an auto-generated devcontainer.json that
+# includes the 'watchman' Feature with no options.
+#
+# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
+#
+# Eg:
+# {
+#    "image": "<..some-base-image...>",
+#    "features": {
+#      "watchman": {}
+#    },
+#    "remoteUser": "root"
+# }
+#
+# These scripts are run as 'root' by default. Although that can be changed
+# with the '--remote-user' flag.
+#
+# This test can be run with the following command:
+#
+#    devcontainer features test    \
+#               --features watchman \
+#               --remote-user root  \
+#               --skip-scenarios    \
+#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
+#               /path/to/this/repo
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
+check "watchman is installed" which watchman
+check "watchman version" watchman --version
+check "watchman executable exists" test -x /usr/local/bin/watchman
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
